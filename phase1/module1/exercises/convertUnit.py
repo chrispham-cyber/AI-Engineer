1cca2565d3b440623e9fe5faeb07c6d2a72c2683
chrispham-cyber <tpham7459@gmail.com>
2026-05-12 18:50:33 -0400

    Exercises: Module convert units

---
+++ phase1/module1/exercises/convertUnit.py
@@ -0,0 +1,15 @@
+def c_to_f(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+
+def f_to_c(fahrenheit):   
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
+
+def km_to_miles(kilometers):
+    miles = kilometers * 0.621371
+    return miles
+
+def miles_to_km(miles):
+    kilometers = miles / 0.621371
+    return kilometers
